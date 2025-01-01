b90df645c2523886f814735f67dad1259956c842
Abhishek Kumar <abhishekkmr031@gmail.com>
2025-01-01 14:57:27 -0800

    header and footer changes changes

---
+++ uiportfolio/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header/Header'
+import Home from './Home/Home'
+import Footer from './Footer/Footer'
+
+function App() {
+  return (
+      <div>
+        <Header/>
+        <Home></Home>
+        <Footer/>
+      </div>
+  );
+}
+
+export default App;
---
+++ uiportfolio/src/Header/Header.js
@@ -0,0 +1,13 @@
+import "./header.css"
+
+function Header() {
+    return (
+
+        <div className="header" >               
+             <a href="">Home</a>
+             <a href="">About</a>               
+        </div>
+    )
+}
+
+export default Header;
