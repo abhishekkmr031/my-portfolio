05d9e9bbdccb92d65da3d1b9a35574c34abc8246
Abhishek Kumar <abhishekkmr031@gmail.com>
2025-01-02 11:24:59 -0800

    added JSON file for fake api

--- uiportfolio/src/Home/Home.js
+++ uiportfolio/src/Home/Home.js
@@ -1,25 +1,79 @@
 import "./Home.css"
 
 function Home(){
+    let timeline="Mar 2022";
+    let title = "Technical Architect";
+    let company = "Devon";
+    let expPointsArray = ["achieved this", "implemented this ", "delivered all this"];
     return(
         <div className="home-container">
-            <p>{photo()}</p>
-            <p>{Objective()}    </p>
+            <div>{NameAddress()}</div>
+            <div>{photo()}</div>
+            <div>{Objective()}</div>
+            <h2>Experience</h2>
+            <div>{ExperienceCard(timeline, title, company, expPointsArray)}</div>
+            <div>{ExperienceCard(timeline, title, company, expPointsArray)}</div>
+            <h2>Certifications</h2>
+            <div>{CertificationCard("AZ-900")}</div>
+            <div>{CertificationCard("PSM-1")}</div>
         </div>
     );
 }
 
+function CertificationCard(certificationName){
+    return(
+        listItem(certificationName, certificationName)
+    )
+}
+
+function ExperienceCard(timeline, title, company, expPointsArray){
+    return(
+        <div className="experience-container">
+            <h4>{timeline}</h4>
+            <span>
+                <h3>{title}, {company}</h3>                
+            </span>
+            <div>
+                {expPointsArray.map((item, i) => {
+                    console.log(item);
+                    return(listItem(i, item))
+                })}
+            </div>             
+        </div>
+    )
+}
+
+function listItem(key, point){
+    const style = {margin:20};
+    return(
+        <li style={style} key={key}>{point}</li>
+    )
+}
+
+function NameAddress(){
+    return (
+        <div className="name-address">
+            <h1> ABHISHEK KUMAR </h1>
+            <h4>Phone 7975142432</h4>
+            <div className="linkedin">
+                <pre><h4>abhishekkmr031@gmail.com <a href="https://www.linkedin.com/in/abhishek-kumar-193369a4/">Linkedin</a></h4> </pre>
+            </div>
+        </div>
+    )
+}
+
 function photo(){
     return(
-        <img className="home-img" src={require("./akuma_image.jpg")} alt="image of person"/>
+        <img className="home-img" src={require("./bluebg.jpeg")} alt="image of person"/>
     )
 }
 
 function Objective(){
     return(
-        <p>
+        <div>
+            <h2>Objective</h2>
             Experienced software developer with a passion for tackling challenges head-on and delivering innovative solutions. With 7 years of industry experience, I thrive in environments where professional challenges excite me and drive my commitment to continuous learning and self-improvement. My proactive approach ensures fast delivery of high-quality solutions, and my ability to independently figure out solutions allows me to excel in dynamic work environments. Seeking opportunities where I can leverage my expertise to contribute to cutting-edge projects while staying updated with the latest technologies and methodologies in software development.
-        </p>
+        </div>
     )
 }
 
